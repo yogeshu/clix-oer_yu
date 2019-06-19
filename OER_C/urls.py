c562a25b82c1a54eaf8137b893ef2ceff86f3608
yogeshu <yogeshbhavsar1994@gmail.com>
2019-06-19 13:01:32 +0530

    some Ui

--- OER_C/urls.py
+++ OER_C/urls.py
@@ -14,14 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from OxE.views import homepage_view, domain_view, about_view , vix_view , demo_view , player_image_view
+from django.urls import path,re_path
+from OxE.views import homepage_view, domain_view, about_view , vix_view , demo_view , player_image_view , module_cards_detail_view, domain_sub_view
 urlpatterns = [
     
 path('', homepage_view , name= "home"),
 path('playerImage', player_image_view , name="player_image_view"),
-path('domain', domain_view ),
+re_path('domain$', domain_view ),
 path('about', about_view),
-path('cscroll/' , demo_view),
+path('cscroll' , demo_view),
+path('module_details' , module_cards_detail_view),
 path('admin/', admin.site.urls),
+re_path('domain/(?P<domain_name>\w+)$', domain_sub_view,name="domain_sub_view")
 ]
--- OxE/views.py
+++ OxE/views.py
@@ -77,8 +77,35 @@ def domain_view(request,  *args, **kwargs):
 
     return render(request , "domain.html" , {'destus':destus})
 def demo_view( request, *args, **kwargs):
+    destu1 = index()
+    destu1.course_value = "math"
+    destu1.coruse_student = "8th"
+    destu1.course_description =  "lorem more more more more "
+    destu1.course_title = "Math courses"
+    destu1.course_math = True
+
+     
+    destu2 = index()
+    destu2.course_value = "science"
+    destu2.coruse_student = "9th"
+    destu2.course_description =  "lorem more"
+    destu2.course_title = "sciene courses"
+    destu2.course_math = False
+     
+    destu3 = index()
+    destu3.course_value = "math"
+    destu3.coruse_student = "8th"
+    destu3.course_description =  "lorem more"
+    destu3.course_title = "scienes courses"
+    destu3.course_math = True
+    
+
+    
+    
+     
+    destus= [ destu1, destu2, destu3 ]
     # return HttpResponse ("<h1> hello i am new demo_view </h1>")
-    return render(request , "cscroll.html", {})
+    return render(request , "cscroll.html", {'destus':destus})
 
 
 def vix_view( request,  *args, **kwargs):
@@ -88,3 +115,10 @@ def vix_view( request,  *args, **kwargs):
 def player_image_view( request, *args , **kwargs):
     return render(request, "player_image.html", {})
 
+def module_cards_detail_view( request , *args , **kwargs):
+    return render(request, "module_cards_detail.html", {})
+
+def domain_sub_view( request , domain_name="maths"):
+    return render(request, "module_cards_detail.html", {})
+
+    return HttpResponse (domain_name)
